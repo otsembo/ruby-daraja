331feeb57a69fe98ad390989fd71298591677f04
otsembo <okumu.otsembo@gmail.com>
2023-04-18 05:00:38 +0300

    Initial commit

---
+++ lib/ruby/daraja/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Daraja
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/ruby/daraja_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruby::Daraja do
+  it "has a version number" do
+    expect(Ruby::Daraja::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
